0b0b51e75ca138e04f903d6b4b7f7aafdc9955a6
anuragrawat <anuragrawat.305@gmail.com>
2023-02-03 12:53:30 +0530

    Initial commit

---
+++ node-app/app.js
@@ -0,0 +1,11 @@
+// const fs = require('fs')
+// fs.writeFileSync('notes.txt', 'Welcome to Node JS.')
+// fs.appendFileSync('notes.txt', ' Data is added')
+
+// import validator from 'validator'
+//console.log(validator.isEmail('anurag@gmail.com'))
+//console.log(validator.isURL('www.google.com))
+
+import chalk from 'chalk'
+console.log(chalk.green('Success'))
+
